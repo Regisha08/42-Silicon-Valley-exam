8e5d0f6b587c1f590d2e62229c075039feca1388
Regina <regishan.08@gmail.com>
2019-10-14 16:32:41 +0200

    add ex

--- level_2/inter.c
+++ level_2/inter.c
@@ -6,61 +6,71 @@
 /*   By: reginanureeva <reginanureeva@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 12:25:51 by reginanuree       #+#    #+#             */
-/*   Updated: 2019/10/14 11:13:12 by reginanuree      ###   ########.fr       */
+/*   Updated: 2019/10/14 16:30:09 by reginanuree      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-//--------------
-int main(int argc, char **argv)
-{
-    int i;
-    int j;
-    i = 0;
-    j = 0;
-    if (argc == 3)
-    {
 
+int chaek(char *str, char a)
+{
+    int x;
+    x = 0;
+    while(str[x])
+    {
+        if (str[x]== a)
+            return(1);
+        x++;
     }
-    write(1,"\n",1);
     return(0);
 }
-// #include <unistd.h>
-// int check( char *str, int i)
-// {
-// 	int j;
-// 	j = 0;
-// 	while (j < i)
-// 	{
-// 		if (str[j]== str[i])
-// 			return(0);
-// 		j++;
-// 	}
-// 	return(1);
-// }
 
-// int main(int argc, char **argv)
-// {
-// 	int i;
-// 	int j;
-
-// 	i = 0;
-// 	j = 0;
-// 	if (argc == 3)
-// 	{
-// 		while (argv[1][i] && argv[2][j])
-// 		{
-// 			if (check(&argv[1][i], i))
-// 			{
-// 				if (argv[1][i] == argv[2][j])
-// 				{
-// 					write (1, &argv[1][i],1);
-// 				}
-// 				j++;
-// 			}
-// 		i++;
-// 		}
-// 	}
-// 	write (1, "\n",1);
-// 	return (0);
-// }
+int main(int argc, char **argv)
+{
+    int a;
+    int b;
+    int c;
+    a = 0;
+    b = 0;
+    c = 0;
+    
+    if (argc ==3)
+    {
+        while(argv[1][a] && c == 0)
+        {
+            if(argv[1][0] == argv[2][b])
+            {
+                write(1,&argv[1][0],1);
+                c = 1;
+            }
+            else 
+                b++;
+        }
+        a++;
+        b = 0;
+        c = 0;
+        while (argv[1][a])
+        {
+            b = 0;
+            while (argv[1][b] != argv[1][a] && b < a)
+            {
+                b++;
+            }
+            b--;
+            if (b == a - 1)
+            {
+                if (chaek(&argv[2][c], argv[1][a]) == 1)
+                {
+                    write(1,&argv[1][a],1);
+                    a++;
+                }
+                else if (chaek(&argv[2][c], argv[1][a]) == 0)
+                    a++;
+            }
+            else
+                a++;
+        }
+    }
+	write (1, "\n",1);
+	return (0);
+}
