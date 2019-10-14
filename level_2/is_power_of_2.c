a663a469961c428c5c7d9e1b1bca58c6480b4d1f
Regina <regishan.08@gmail.com>
2019-10-14 09:06:13 +0200

    add new

--- level_2/is_power_of_2.c
+++ level_2/is_power_of_2.c
@@ -6,13 +6,33 @@
 /*   By: reginanureeva <reginanureeva@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 13:47:01 by reginanuree       #+#    #+#             */
-/*   Updated: 2019/10/12 13:48:01 by reginanuree      ###   ########.fr       */
+/*   Updated: 2019/10/12 13:58:18 by reginanuree      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int	    is_power_of_2(unsigned int n)
 {
-    int i;
-    i = 0;
+    unsigned int i;
+    i = 1;
     
+    if(n == 0)
+        return(0);
+    if(n == 1)
+        return(1);
+    while(i<n)
+    {
+        i = i * 2;
+        if (i == n)
+            return(1);
+    }
+    return(0);
+}
+int main()
+{
+    int h;
+    h = 32;
+    printf("%d\n", is_power_of_2(h));
+    return(0);
 }
---
+++ level_2/last_word.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   last_word.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reginanureeva <reginanureeva@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/12 13:59:54 by reginanuree       #+#    #+#             */
+/*   Updated: 2019/10/12 23:22:20 by reginanuree      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    int i;
+    i = 0;
+    if(argc == 2)
+    {
+        while(argv[1][i])
+            i++;
+        i--;
+        while(argv[1][i] == ' ' || argv[1][i] == '\t')
+            i--;
+        while(argv[1][i] != ' ' || argv[1][i] == '\t')
+            i--;
+        i++;
+        while((argv[1][i] != ' ' && argv[1][i] != '\t')&& argv[1][i])
+        {
+            write(1,&argv[1][i],1);
+            i++;
+        }
+    }
+    write(1,"\n",1);
+    return(0);
+}
