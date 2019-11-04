d70ab610ec27e5be8cf6007e60519dc518150aa7
Regina <regishan.08@gmail.com>
2019-11-04 11:20:39 -0800

    fix

--- level_3/ft_range.c
+++ level_3/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: reginanureeva <reginanureeva@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/15 12:09:49 by reginanuree       #+#    #+#             */
-/*   Updated: 2019/10/15 12:28:54 by reginanuree      ###   ########.fr       */
+/*   Updated: 2019/11/04 11:19:31 by reginanuree      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,50 @@
 
 int     *ft_range(int start, int end)
 {
-    char *s;
-    
-    if ()
-    
+    int *s;
+    int num;
+    int i;
+    int step;
+
+    if (start > end)
+      num = start - end +1;
+    else
+      num = end - start +1;
+    s = malloc(sizeof(int) * num);
+    if (start < end)
+      step = 1;
+    else
+      step = -1;
+    while(i < num)
+    {
+      s[i]= start;
+      start = start + step;
+      i++;
+    }
+    return(s);
 }
 int main()
 {
-    int start;
-    int end;
-    int *res;
-    int z;
-    int d;
-    z = 0;
-    start = 0;
-    end = -3;
-    if (end < 0)
-      d = (end *(-1) - start)+1;
-    else
-      d = (end - start) +1;
-    res = ft_range(start, end);
-    while(z < d)
-    {
-        printf("%d, ",res[z]);
-        z++;
-    }
-    return(0);
+  int start;
+  int end;
+  int *arr;
+  int i;
+  int num;
+  num = 0;
+
+  start = 0;
+  end = -3;
+  i = 0;
+  if (start > end)
+      num = start - end +1;
+  else
+      num = end - start +1;
+  arr = ft_range(start,end);
+  while(i < num)
+  {
+    printf("%d, ",arr[i]);
+    i++;
+  }
+  printf("\n");
+  return(0);
 }
